940ac6b29cf6eb88c28865c7f0988c7941e90ac3
Cyril Lagger <cyril.lagger@liverpool.ac.uk>
2020-04-06 13:31:42 +0100

    test CellPhoneDB

---
+++ tests/testthat/test-create_cpdb_input.R
@@ -0,0 +1,8 @@
+# test_that("create_cpdb_input works", {
+#   create_cpdb_input(seurat_obj = seurat_random_test, assay = "SCT",
+#                     slot = "counts",
+#                     log_scale = FALSE,
+#                     seurat_cell_type_id = "cell_ontology_class",
+#                     condition_id = NULL,
+#                     input_dir = paste0(getwd(),'/tests'))
+# })
