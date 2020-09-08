b471b9fa8ef8af31c5f3ef86626db024491f3dfa
Cyril Lagger <lagger.cyril@gmail.com>
2020-09-08 13:23:24 +0100

    Updated LR data

---
+++ R/LR6db_wSource_data.R
@@ -0,0 +1,15 @@
+#' Ligand-receptor interactions from 6 databases.
+#'
+#' LR6db_wSource contains a data.table of LR interactions and a list of the sources from each database.
+#' @docType data
+#'
+#' @usage data(LR6db_wSource)
+#'
+#' @format A list of 1 data.table and one list.
+#'
+#' @keywords datasets
+#'
+#' @references to add
+#' (\href{to_add}{PubMed})
+#'
+"LR6db_wSource"
