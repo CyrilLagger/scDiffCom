d683c55f49f7e5372fb646e2fb55f1033fa9e3a9
Cyril Lagger <lagger.cyril@gmail.com>
2021-04-25 14:33:43 +0100

    Showing how LRI datasets are built

---
+++ data-raw/LRI_human.R
@@ -0,0 +1,5 @@
+## code to prepare `LRI_human` dataset
+
+#LRI_human <- build_LRI(species = "human")
+
+#usethis::use_data(LRI_human, overwrite = TRUE)
---
+++ data-raw/LRI_mouse.R
@@ -0,0 +1,5 @@
+## code to prepare `LRI_mouse` dataset
+
+#LRI_mouse <- build_LRI(species = "mouse")
+
+#usethis::use_data(LRI_mouse, overwrite = TRUE)
