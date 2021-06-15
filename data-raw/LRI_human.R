097d60b0f2fca84a2522b3e004fe10ded9c83917
Cyril Lagger <lagger.cyril@gmail.com>
2021-06-15 13:50:22 +0100

    Minor code refactoring

--- data-raw/LRI_human.R
+++ data-raw/LRI_human.R
@@ -1,5 +1,4 @@
 ## code to prepare the `LRI_human` dataset
 
 #LRI_human <- build_LRI(species = "human")
-
 #usethis::use_data(LRI_human, overwrite = TRUE)
--- data-raw/LRI_mouse.R
+++ data-raw/LRI_mouse.R
@@ -1,5 +1,4 @@
 ## code to prepare the `LRI_mouse` dataset
 
 #LRI_mouse <- build_LRI(species = "mouse")
-
 #usethis::use_data(LRI_mouse, overwrite = TRUE)
